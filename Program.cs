3662027a6de927a7cffcb3bfbc5359be3c2127a7
roman <roman3668@gmail.com>
2022-11-03 11:31:06 +0300

    Add Task4

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
-﻿Console.WriteLine ("Enter number ");
+﻿Console.WriteLine ("Enter number greater than 1 ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num%2 == 0){
-    Console.WriteLine($"{num} is even number");
+int i = 2;
+
+if (num > 0 && num%2 == 0){
+    while (i<num){
+    Console.Write ($"{i}, ");
+    i += 2;
+    }
+    Console.WriteLine (num);
+}
+if (num > 0 && num%2 != 0){
+    while (i<num){
+    Console.Write ($"{i}, ");
+    i += 2;
+    }
 }
 else{
-    Console.WriteLine($"{num} is not even number");
+    Console.WriteLine ("Enter number greater than 1");
 }
+
+

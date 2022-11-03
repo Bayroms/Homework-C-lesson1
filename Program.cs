173a98a227f7e3fc88dd680d5748a6e9b2a5f8af
roman <roman3668@gmail.com>
2022-11-03 10:49:17 +0300

    Added Task1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Enter two numbers ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB){
+    Console.WriteLine ($"{numA} greater then {numB}");
+}
+else if (numA == numB){
+    Console.WriteLine ("Enter two different numbers");
+}
+else {
+    Console.WriteLine ($"{numB} greater then {numA}");
+}
